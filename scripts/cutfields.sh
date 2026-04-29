ede47b8d1b49948dc5f5dad036c024413486fa07
Kyle Versluis <ktalonsec@gmail.com>
2026-04-29 01:51:00 -0700

    Add files via upload

--- scripts/cutfields.sh
+++ scripts/cutfields.sh
@@ -1,24 +1,35 @@
-tshark -r <filename>.pcap -Y "ip" -T fields \
--e frame.time_epoch \
--e frame.len \
--e frame.protocols \
--e ip.src \
--e ip.dst \
--e ip.proto \
--e tcp.srcport \
--e tcp.dstport \
--e tcp.flags \
--e udp.srcport \
--e udp.dstport \
--e ntp.stratum \
--e ntp.ppoll \
--e ntp.precision \
--e ntp.rootdelay \
--e ntp.rootdispersion \
--e ntp.refid \
--e ntp.org \
--e ntp.rec \
--e ntp.xmt \
--E header=y \
--E separator=, \
--E occurrence=f > <filename>.csv
+#!/usr/bin/env bash
+# cutfields.sh — extract IP-based packet fields from a PCAP into a CSV.
+#
+# Edit the two values below, then run:
+#   bash cutfields.sh
+#
+# The CSV produced here is the input to cleandata.py.
+
+INPUT_PCAP="input.pcap"
+OUTPUT_CSV="output.csv"
+
+tshark -r "$INPUT_PCAP" -Y "ip" -T fields \
+  -e frame.time_epoch \
+  -e frame.len \
+  -e frame.protocols \
+  -e ip.src \
+  -e ip.dst \
+  -e ip.proto \
+  -e tcp.srcport \
+  -e tcp.dstport \
+  -e tcp.flags \
+  -e udp.srcport \
+  -e udp.dstport \
+  -e ntp.stratum \
+  -e ntp.ppoll \
+  -e ntp.precision \
+  -e ntp.rootdelay \
+  -e ntp.rootdispersion \
+  -e ntp.refid \
+  -e ntp.org \
+  -e ntp.rec \
+  -e ntp.xmt \
+  -E header=y \
+  -E separator=, \
+  -E occurrence=f > "$OUTPUT_CSV"
